f4c6eea0948753972afaf9669d57c6e344e4a8e9
almeida <hello@eualmeida.com.br>
2026-01-17 13:49:14 -0300

    feat(@abacatepay): add JSDoc comments in version and API_VERSION constants

--- packages/rest/src/version.ts
+++ packages/rest/src/version.ts
@@ -1,4 +1,10 @@
 // AUTO-GENERATED — DO NOT EDIT
 
+/**
+ * Current version of [@abacatepay/rest](https://www.npmjs.com/package/@abacatepay/rest).
+ */
 export const version = '0.0.1' as const;
+/**
+ * Current version of the AbacatePay API
+ */
 export const API_VERSION = '2' as const;
