04a6d020c4ea27ae5f5b9aa12c231cc778593c6e
almeida <hello@eualmeida.com.br>
2026-01-21 11:00:39 -0300

    feat(@abacatepay/zod): add v1 charge schemas

---
+++ packages/zod/src/v1/resources/charge.ts
@@ -0,0 +1,126 @@
+import { z } from 'zod';
+import { StringEnum } from '../../utils';
+import { APICustomer } from './customer';
+
+/**
+ * https://docs.abacatepay.com/pages/payment/reference#atributos
+ */
+export const PaymentStatus = StringEnum(
+	['PENDING', 'EXPIRED', 'CANCELLED', 'PAID', 'REFUNDED'],
+	'Billing status. Can be `PENDING`, `EXPIRED`, `CANCELLED`, `PAID`, `REFUNDED`.',
+);
+
+export type PaymentStatus = z.infer<typeof PaymentStatus>;
+
+/**
+ * https://docs.abacatepay.com/pages/payment/create#body-methods
+ */
+export const PaymentMethod = StringEnum(['PIX', 'CARD'], 'Payment method.');
+
+export type PaymentMethod = z.infer<typeof PaymentMethod>;
+
+/**
+ * https://docs.abacatepay.com/pages/payment/create#body-frequency
+ */
+export const PaymentFrequency = StringEnum(
+	['ONE_TIME', 'MULTIPLE_PAYMENTS'],
+	'Payment frequency.',
+);
+
+export type PaymentFrequency = z.infer<typeof PaymentFrequency>;
+
+export const APIProduct = z.object({
+	externalId: z.string({
+		description:
+			'The product id on your system. We use this id to create your product on AbacatePay automatically, so make sure your id is unique.',
+	}),
+	name: z.string({
+		description: 'Product name.',
+	}),
+	quantity: z
+		.number({
+			description: 'Quantity of product being purchased',
+		})
+		.int()
+		.min(1),
+	price: z
+		.number({
+			description:
+				'Price per unit of product in cents. The minimum is 100 (1 BRL).',
+		})
+		.int()
+		.min(100),
+	description: z
+		.string({
+			description: 'Detailed product description.',
+		})
+		.optional(),
+});
+
+/**
+ * https://docs.abacatepay.com/pages/payment/reference#estrutura
+ */
+export const APICharge = z.object({
+	id: z.string({
+		description: 'Unique billing identifier.',
+	}),
+	frequency: PaymentFrequency,
+	externalId: z.nullable(z.string({ description: 'Bill ID in your system.' })),
+	url: z
+		.string({
+			description: 'URL where the user can complete the paymenz.',
+		})
+		.url(),
+	status: PaymentStatus,
+	devMode: z.boolean({
+		description:
+			'Indicates whether the charge was created in a development (true) or production (false) environmenz.',
+	}),
+	metadata: z.object({
+		fee: z.number({
+			description: 'Fee applied by AbacatePay.',
+		}),
+		returnUrl: z
+			.string({
+				description:
+					'URL that the customer will be redirected to when clicking the “back” button.',
+			})
+			.url(),
+		completionUrl: z
+			.string({
+				description:
+					'URL that the customer will be redirected to when making paymenz.',
+			})
+			.url(),
+	}),
+	methods: PaymentMethod.array(),
+	products: z
+		.array(APIProduct, {
+			description: 'List of products included in the charge.',
+		})
+		.min(1),
+	customer: APICustomer.optional(),
+	nextBilling: z.nullable(z.coerce.date(), {
+		description: 'Date and time of next charge, or null for one-time charges.',
+	}),
+	allowCoupons: z
+		.boolean({
+			description: 'Whether or not to allow coupons for billing.',
+		})
+		.optional(),
+	coupons: z
+		.array(z.string(), {
+			description:
+				'Coupons allowed in billing. Coupons are only considered if `allowCoupons` is true.',
+		})
+		.min(50)
+		.default([]),
+	createdAt: z.coerce.date({
+		description: 'Charge creation date and time.',
+	}),
+	updatedAt: z.coerce.date({
+		description: 'Charge last updated date and time.',
+	}),
+});
+
+export type APICharge = z.infer<typeof APICharge>;
