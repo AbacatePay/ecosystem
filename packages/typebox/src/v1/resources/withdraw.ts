ec254ba61617ed1d03ab3376d6cfa9516beb30bb
almeida <hello@eualmeida.com.br>
2026-01-21 09:24:34 -0300

    feat(@abacatepay/typebox): add v1 withdraw schemas

---
+++ packages/typebox/src/v1/resources/withdraw.ts
@@ -0,0 +1,47 @@
+import { type Static, Type as t } from '@sinclair/typebox';
+import { StringEnum } from '../../utils';
+
+/**
+ * https://docs.abacatepay.com/pages/withdraw/reference#atributos
+ */
+export const WithdrawStatus = StringEnum(
+	['PENDING', 'EXPIRED', 'CANCELLED', 'COMPLETE', 'REFUNDED'],
+	'Transaction status.',
+);
+
+export type WithdrawStatus = Static<typeof WithdrawStatus>;
+
+/**
+ * https://docs.abacatepay.com/pages/withdraws/reference
+ */
+export const APIWithdraw = t.Object({
+	id: t.String({
+		description: 'Unique transaction identifier.',
+	}),
+	status: WithdrawStatus,
+	devMode: t.Boolean({
+		description:
+			'Indicates whether the transaction was created in a testing environment.',
+	}),
+	receiptUrl: t.String({ format: 'uri', description: 'Transaction proof URL.',}),
+	amount: t.Integer({
+		description: 'Withdraw value in cents.',
+	}),
+	platformFee: t.Integer({
+		description: 'Platform fee in cents.',
+	}),
+	externalId: t.Optional(t.String({
+		description: 'External transaction identifier.',
+	})),
+	createdAt: t.Date({
+		description: 'Transaction creation date.',
+	}),
+	updatedAt: t.Date({
+		description: 'Transaction update date.',
+	}),
+	kind: t.Literal('WITHDRAW', {
+		description: 'Transaction type. It will always be \'WITHDRAW\'',
+	}),
+});
+
+export type APIWithdraw = Static<typeof APIWithdraw>;
