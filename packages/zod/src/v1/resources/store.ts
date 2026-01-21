3aa552ecf615c5417365153252a08ccffbca5619
almeida <hello@eualmeida.com.br>
2026-01-21 11:07:02 -0300

    feat(@abacatepay/zod): add v1 store schemas

---
+++ packages/zod/src/v1/resources/store.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+/**
+ * https://docs.abacatepay.com/pages/store/reference#estrutura
+ */
+export const APIStore = z.object({
+	id: z.string({
+		description: 'Unique identifier for your store on AbacatePay.',
+	}),
+	name: z.string({
+		description: 'Name of your store/company.',
+	}),
+	balance: z.object(
+		{
+			available: z
+				.number({
+					description: 'Balance available for withdrawal in cents.',
+				})
+				.int(),
+			pending: z
+				.number({
+					description: 'Balance pending confirmation in cents.',
+				})
+				.int(),
+			blocket: z
+				.number({
+					description: 'Balance blocked in disputes in cents.',
+				})
+				.int(),
+		},
+		{
+			description: 'Object containing information about your account balances.',
+		},
+	),
+});
+
+export type APIStore = z.infer<typeof APIStore>;
