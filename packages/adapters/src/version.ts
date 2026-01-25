cc6e9df4afbd6b7a774dfc5d6235a52b7340893e
almeida <hello@eualmeida.com.br>
2026-01-25 09:27:27 -0300

    feat(@abacatepay/adapters): relase v1.0.0

---
+++ packages/adapters/src/version.ts
@@ -0,0 +1,6 @@
+// AUTO-GENERATED — DO NOT EDIT
+
+/**
+ * Current version of [@abacatepay/adapters](https://www.npmjs.com/package/@abacatepay/adapters).
+ */
+export const version = '1.0.0' as const;
